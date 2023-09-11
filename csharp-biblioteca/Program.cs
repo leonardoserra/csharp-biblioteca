f71672996498828a5d67efb3134edd0e08cd91dd
leonardoserra <serra.leonardo92@gmail.com>
2023-09-11 14:54:41 +0200

    creata classe User e prova di stampa to string su istanza User

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            User gimmighione = new User("Ghione", "Gimmi", "gghione@gmail.ghione", "castoroMolesto2023!","392-2378755");
+            Console.WriteLine(gimmighione.ToString());
         }
     }
 }
---
+++ csharp-biblioteca/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class User
+    {
+        public string LastName { get; private set; }
+        public string Name{ get; private set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(string lastName, string name, string email, string password, string phoneNumber)
+        {
+            this.LastName = lastName;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = phoneNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Heilà amici di Striscia, da Cologno Monzese è tutto! Io sono: {this.LastName} {this.Name}!\nLa mia email è: {this.Email} e la mia password é: {this.Password}...\nVideochiamami! -> {this.PhoneNumber}";
+        }
+    }
+}
