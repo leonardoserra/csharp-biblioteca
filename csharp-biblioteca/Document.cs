82859c4e40089541ad3f9d677522ec5b1f6ddac1
leonardoserra <serra.leonardo92@gmail.com>
2023-09-11 15:11:08 +0200

    creata Document class con prova di stampa toString()

---
+++ csharp-biblioteca/Document.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Document
+    {
+        public string IdCode { get; private set; }
+        public string Title { get;  set; }
+        public uint ProductionYear { get; set; }
+        public string Sector { get; set; }
+        public uint Rack { get; set; }
+        public string Author { get; set; }
+
+        public Document(string title, uint productionYear, string sector, uint rack, string author) {
+            this.IdCode = "DOCUMENT n." + Random.Shared.Next(0, 99999).ToString();
+            this.Title = title;
+            this.ProductionYear = productionYear;
+            this.Sector = sector;
+            this.Rack = rack;
+            this.Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"Info documento: \n\tCodice: {this.IdCode}\n\tTitolo: {this.Title}\n\tAnno di produzione: {this.ProductionYear}\n\tGenere/Settore: {this.Sector}\n\tScaffale: {this.Rack}\n\tAutore: {this.Author}";
+        }
+    }
+}
--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -6,6 +6,10 @@
         {
             User gimmighione = new User("Ghione", "Gimmi", "gghione@gmail.ghione", "castoroMolesto2023!","392-2378755");
             Console.WriteLine(gimmighione.ToString());
+
+            Document servizioStrisciaLaNotizia = new Document("Cuccioli di cinghiale a Cologno Monzese (feat.Boldi)", 1992, "Cronaca Comica", 78, "Splash Editorial");
+            Console.WriteLine(servizioStrisciaLaNotizia.ToString());
+
         }
     }
 }
